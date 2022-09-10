a272f9a692bbbf906303d2ed55c01fb851c8a5d7
Lucas <lucasalexandre_82@hotmail.com>
2022-09-10 12:22:50 -0300

    Concluindo primeira parte do arquivo JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
-console.log("teste");
+// Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Funções (letras, Números e Símbolos)
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/\,.!@#$%&*+-_"
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getSymbol());
+
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log("Teste");
+})
